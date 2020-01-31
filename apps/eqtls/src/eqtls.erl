d6c033d27bfbfded29f0f7d8cccdad75f578f0e9
Péter Dimitrov <peterdmv@erlang.org>
2020-01-31 10:27:15 +0100

    Implement ClientHello

--- apps/eqtls/src/eqtls.erl
+++ apps/eqtls/src/eqtls.erl
@@ -1,10 +1,36 @@
 -module(eqtls).
 
--export([client/0]).
+-export([client/0,
+	 server/0,
+	 get_flight/1,
+	 put_flight/2,
+	 set_transport_parameters/2]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
 client() ->
-    eqtls_sup:start_child([]).
+    eqtls_sup:start_child([client]).
+
+server() ->
+    eqtls_sup:start_child([server]).
+
+%% Including transport parameters in the TLS handshake provides
+%% integrity protection for these values.
+
+%%    enum {
+%%       quic_transport_parameters(0xffa5), (65535)
+%%    } ExtensionType;
+
+%% The "extension_data" field of the quic_transport_parameters extension
+%% contains a value that is defined by the version of QUIC that is in
+%% use.
+set_transport_parameters(_Pid, _Params) ->
+    ok.
+
+get_flight(Pid) ->
+    eqtls_statem:get_flight(Pid).
+
+put_flight(_Pid, _Handshake) ->
+    ok.
--- apps/eqtls/src/eqtls_statem.erl
+++ apps/eqtls/src/eqtls_statem.erl
@@ -3,29 +3,42 @@
 -behaviour(gen_statem).
 
 %% API
--export([start_link/0]).
+-export([get_flight/1,
+	 start_link/1]).
 
 %% gen_statem callbacks
 -export([init/1, callback_mode/0, terminate/3]).
 
 %% gen_statem state functions
--export([start/3]).
+-export([negotiated/3,
+	 start/3,
+	 wait_sh/3]).
 
--record(state, {}).
+-record(state, {mode,
+		history = []}).
+
+
+-include_lib("ssl/src/ssl_handshake.hrl").
+-include_lib("ssl/src/ssl_internal.hrl").
+-include_lib("ssl/src/tls_handshake_1_3.hrl").
+-include_lib("public_key/include/public_key.hrl").
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
-start_link() ->
-    gen_statem:start_link({local,?MODULE}, ?MODULE, [], []).
+start_link(Mode) ->
+    gen_statem:start_link(?MODULE, [Mode], []).
+
+get_flight(Pid) ->
+    call(Pid, {get_flight}).
 
 %%====================================================================
 %% gen_statem callback functions
 %%====================================================================
 
-init([]) ->
-    {ok, start, #state{}}.
+init([Mode]) ->
+    {ok, start, #state{mode = Mode}}.
 
 callback_mode() ->
     state_functions.
@@ -37,9 +50,91 @@ terminate(_Reason, _State, _Data) ->
 %% gen_statem state functions
 %%====================================================================
 
-start(_, _, State) ->
-    {next_state, start, State}.
+start({call, From}, {get_flight}, #state{mode = client} = StateData) ->
+    ClientHello = client_hello(StateData),
+    {next_state, wait_sh, StateData, [{reply, From, ClientHello}]};
+start(cast, {put_flight, [Raw]}, #state{mode = server,
+					history = History} = StateData0) ->
+    Version = {3,4},
+    <<?BYTE(Type), ?UINT24(_), Body/binary>> = Raw,
+    _ClientHello = tls_handshake:decode_handshake(Version, Type, Body),
+    %% TODO: process ClientHello
+    {next_state, negotiated, StateData0#state{history = [Raw|History]}};
+start(_, _, StateData) ->
+    {next_state, start, StateData}.
+
+wait_sh({call, From}, _, StateData) ->
+    {next_state, start, StateData, [{reply, From, restarted}]}.
+
+negotiated({call, From}, {get_flight}, #state{mode = server} = StateData) ->
+    {next_state, wait_cert, StateData, [{reply, From, <<"SH EE Cert CV F">>}]}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+call(FsmPid, Event) ->
+    try gen_statem:call(FsmPid, Event)
+    catch
+ 	exit:{noproc, _} ->
+ 	    {error, closed};
+	exit:{normal, _} ->
+	    {error, closed};
+	exit:{{shutdown, _},_} ->
+	    {error, closed}
+    end.
+
+client_hello(_StateData) ->
+    Version = {3,4},
+    LegacyVersion = {3,3},
+    AvailableCipherSuites = ssl_cipher:filter_suites(ssl_cipher:suites(Version)),
+    Groups = ssl:groups(),
+    KeyShare = ssl_cipher:generate_client_shares(Groups),
+    #key_share_client_hello{client_shares = ClientShares0} = KeyShare,
+    %% Keep only public keys
+    ClientShares = lists:map(fun kse_remove_private_key/1, ClientShares0),
+
+
+    SessionId = crypto:strong_rand_bytes(32),
+    SignatureSchemes = tls_v1:default_signature_algs(Version),
+    %% TODO: ticket data, handshake history, alpn
+    Extensions = #{sni => #sni{hostname = "localhost"},
+		   ec_point_formats =>
+		       #ec_point_formats{ec_point_format_list = [?ECPOINT_UNCOMPRESSED]},
+		   elliptic_curves =>
+		       #supported_groups{supported_groups = Groups},
+		   signature_algs =>
+		       #signature_algorithms{signature_scheme_list = SignatureSchemes},
+		   client_hello_versions =>
+		       #client_hello_versions{versions = [Version]},
+		   signature_algs_cert =>
+		       #signature_algorithms_cert{signature_scheme_list = SignatureSchemes},
+		   key_share =>
+		       #key_share_client_hello{client_shares = ClientShares}
+		  },
+
+    ClientHello0 = #client_hello{session_id = SessionId,
+				client_version = LegacyVersion,
+				cipher_suites = AvailableCipherSuites,
+				compression_methods = [0],
+				random = crypto:strong_rand_bytes(32),
+				extensions = Extensions
+			       },
+    ClientHello = tls_handshake:encode_handshake(ClientHello0, Version),
+    [iolist_to_binary(ClientHello)].
+
+
+kse_remove_private_key(#key_share_entry{
+                      group = Group,
+                      key_exchange =
+                          #'ECPrivateKey'{publicKey = PublicKey}}) ->
+    #key_share_entry{
+       group = Group,
+       key_exchange = PublicKey};
+kse_remove_private_key(#key_share_entry{
+                      group = Group,
+                      key_exchange =
+                          {PublicKey, _}}) ->
+    #key_share_entry{
+       group = Group,
+       key_exchange = PublicKey}.
